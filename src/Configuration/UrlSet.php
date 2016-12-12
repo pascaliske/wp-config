5f07bfd53515927fd6be466288ccfdd2e0024b42
Pascal Iske <p.iske@syzygy.de>
2016-12-12 23:50:35 +0100

    Added documentation

--- src/Configuration/UrlSet.php
+++ src/Configuration/UrlSet.php
@@ -2,10 +2,39 @@
 namespace PI\Configuration;
 
 class UrlSet {
+	/* Constants ------------------------------------------------------------------------- */
+
+	/**
+	 * Production URLs
+	 *
+	 * @access private
+	 * @var [Array]
+	 */
 	private $production;
+
+	/**
+	 * Staging URLs
+	 *
+	 * @access private
+	 * @var [Array]
+	 */
 	private $staging;
+
+	/**
+	 * Development URLs
+	 *
+	 * @access private
+	 * @var [Array]
+	 */
 	private $development;
 
+	/* Constructor ----------------------------------------------------------------------- */
+
+	/**
+	 * Instanciates a new url set with default urls
+	 *
+	 * @return [void]
+	 */
 	public function __construct() {
 		$this->environment['production'] = array();
 		$this->environment['staging'] = array(
@@ -17,15 +46,33 @@ class UrlSet {
 		);
 	}
 
-	public function set($key='production', $urls) {
+	/* Public ---------------------------------------------------------------------------- */
+
+	/**
+	 * Sets new urls to an environment
+	 *
+	 * @access public
+	 * @param [String] $env
+	 * @param [Mixed] $urls
+	 * @return [void]
+	 */
+	public function set(String $env='production', $urls) {
 		if (!is_array($urls)) {
 			$urls = array($urls);
 		}
 
-		$this->environment[$key] = array_merge($this->environment[$key], $urls);
+		// merge with existing values
+		$this->environment[$env] = array_merge($this->environment[$env], $urls);
 	}
 
-	public function get($key='production') {
-		return $this->environment[$key];
+	/**
+	 * Returns urls of an environment
+	 *
+	 * @access public
+	 * @param  [String] $key
+	 * @return [Array]
+	 */
+	public function get(String $env='production') {
+		return $this->environment[$env];
 	}
 }

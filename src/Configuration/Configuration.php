6bcce4e9e0eceaea2c5416c6a4bddf254d122e5b
Pascal Iske <info@pascal-iske.de>
2017-12-19 07:51:40 +0100

    refactor: convert indentation to spaces and improve code style

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -4,183 +4,182 @@ namespace PI\Configuration;
 use Symfony\Component\Yaml\Yaml;
 
 class Configuration {
-	/** --- constants --- **/
+    /** --- constants --- **/
 
-	/**
-	 * The env variables
-	 *
-	 * @access private
-	 * @var [Array]
-	 */
-	private $argv;
+    /**
+     * The env variables.
+     *
+     * @access private
+     * @var array
+     */
+    private $argv;
 
-	/**
-	 * The root path
-	 *
-	 * @access private
-	 * @var [String]
-	 */
-	private $rootPath;
+    /**
+     * The root path.
+     *
+     * @access private
+     * @var string
+     */
+    private $rootPath;
 
-	/**
-	 * The urls
-	 *
-	 * @access private
-	 * @var [UrlSet]
-	 */
-	private $urls;
+    /**
+     * The urls.
+     *
+     * @access private
+     * @var UrlSet
+     */
+    private $urls;
 
-	/**
-	 * The options
-	 *
-	 * @access private
-	 * @var [Array]
-	 */
-	private $options;
+    /**
+     * The options.
+     *
+     * @access private
+     * @var array
+     */
+    private $options;
 
-	/**
-	 * The resolved hostname
-	 *
-	 * @access private
-	 * @var [String]
-	 */
-	private $hostname;
+    /**
+     * The resolved hostname.
+     *
+     * @access private
+     * @var string
+     */
+    private $hostname;
 
-	/**
-	 * The resolved environment
-	 *
-	 * @access private
-	 * @var [String]
-	 */
-	private $environment;
+    /**
+     * The resolved environment.
+     *
+     * @access private
+     * @var string
+     */
+    private $environment;
 
     /** --- constructor --- **/
 
-	/**
-	 * Instanciates configuration helper
-	 *
-	 * @access public
-	 * @param [String] $rootPath
-	 * @param [UrlSet] $urls
-	 * @return [void]
-	 */
-	public function __construct($rootPath, UrlSet $urls) {
-		global $argv;
+    /**
+     * Instanciates configuration helper.
+     *
+     * @access public
+     * @param string $rootPath
+     * @param UrlSet $urls
+     * @return void
+     */
+    public function __construct($rootPath, UrlSet $urls) {
+        global $argv;
 
-		$this->argv = $argv ?: array();
-		$this->rootPath = $rootPath;
-		$this->urls = $urls;
+        $this->argv = $argv ?: array();
+        $this->rootPath = $rootPath;
+        $this->urls = $urls;
 
-		$this->options = array();
-		$this->resolveHostname();
-		$this->resolveEnvironment();
-
-		$this->resolveConfig();
-	}
+        $this->options = array();
+        $this->resolveHostname();
+        $this->resolveEnvironment();
+        $this->resolveConfig();
+    }
 
     /** --- private --- **/
 
-	/**
-	 * Resolves the hostname
-	 *
-	 * @access private
-	 * @return [void]
-	 */
-	private function resolveHostname() {
-		$hostname = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
+    /**
+     * Resolves the hostname.
+     *
+     * @access private
+     * @return void
+     */
+    private function resolveHostname() {
+        $hostname = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
 
-		// fetch hostname
-		if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && !empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-			$hostname = $_SERVER['HTTP_X_FORWARDED_HOST'];
-		}
+        // fetch hostname
+        if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && !empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+            $hostname = $_SERVER['HTTP_X_FORWARDED_HOST'];
+        }
 
-		// are we in a cli?
-		if ((PHP_SAPI == 'cli' || php_sapi_name() == 'cli')) {
-			$hostname = 'cli';
-		}
+        // are we in a cli?
+        if ((PHP_SAPI == 'cli' || php_sapi_name() == 'cli')) {
+            $hostname = 'cli';
+        }
 
-		$this->hostname = $hostname;
-		$this->options['hostname'] = $hostname;
-	}
+        $this->hostname = $hostname;
+        $this->options['hostname'] = $hostname;
+    }
 
-	/**
-	 * Resolves the environment
-	 *
-	 * @access private
-	 * @return [void]
-	 */
-	private function resolveEnvironment() {
-		$environment = 'production';
+    /**
+     * Resolves the environment.
+     *
+     * @access private
+     * @return void
+     */
+    private function resolveEnvironment() {
+        $environment = 'production';
 
-		// try env var
-		if (getenv('WP_ENV') !== false) {
-			$environment = preg_replace('/[^a-z]/', '', getenv('WP_ENV'));
-		}
+        // try env var
+        if (getenv('WP_ENV') !== false) {
+            $environment = preg_replace('/[^a-z]/', '', getenv('WP_ENV'));
+        }
 
-		// try env from hostnames
-		foreach (array('production', 'staging', 'development') as $env) {
-			if (in_array($this->hostname, $this->urls->get($env))) {
-				$environment = $env;
-			}
-		}
+        // try env from hostnames
+        foreach (array('production', 'staging', 'development') as $env) {
+            if (in_array($this->hostname, $this->urls->get($env))) {
+                $environment = $env;
+            }
+        }
 
-		// try cli arguments
-		if ((PHP_SAPI == 'cli' || php_sapi_name() == 'cli')) {
-			foreach ($this->argv as $arg) {
-				if (preg_match('/--env=(.+)/', $arg, $match)) {
-					$environment = $match[1];
-				}
-			}
-		}
+        // try cli arguments
+        if ((PHP_SAPI == 'cli' || php_sapi_name() == 'cli')) {
+            foreach ($this->argv as $arg) {
+                if (preg_match('/--env=(.+)/', $arg, $match)) {
+                    $environment = $match[1];
+                }
+            }
+        }
 
-		$this->environment = $environment;
-		$this->options['environment'] = $environment;
-	}
+        $this->environment = $environment;
+        $this->options['environment'] = $environment;
+    }
 
-	/**
-	 * Resolves all config files
-	 *
-	 * @access private
-	 * @return [void]
-	 */
-	private function resolveConfig() {
-		$data = array();
-		$files = sprintf('%s/conf/%s/*.yml', $this->rootPath, $this->environment);
+    /**
+     * Resolves all config files.
+     *
+     * @access private
+     * @return void
+     */
+    private function resolveConfig() {
+        $data = array();
+        $files = sprintf('%s/conf/%s/*.yml', $this->rootPath, $this->environment);
 
-		foreach (glob($files) as $file) {
-			$filename = explode('/', $file);
-			$filename = str_replace('.yml', '', end($filename));
-			$data[$filename] = Yaml::parse(file_get_contents($file));
-		}
+        foreach (glob($files) as $file) {
+            $filename = explode('/', $file);
+            $filename = str_replace('.yml', '', end($filename));
+            $data[$filename] = Yaml::parse(file_get_contents($file));
+        }
 
-		$this->options = array_merge($this->options, $data);
-	}
+        $this->options = array_merge($this->options, $data);
+    }
 
     /** --- public --- **/
 
-	/**
-	 * Returns the value for the given key
-	 *
-	 * @access public
-	 * @param  [String] $option
-	 * @param  [Mixed] $default
-	 * @return [Mixed]
-	 */
-	public function get($option, $default=null) {
-		if (strpos($option, ':') != false) {
-			$options = $this->options;
+    /**
+     * Returns the value for the given key.
+     *
+     * @access public
+     * @param  string $option
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function get($option, $default = null) {
+        if (strpos($option, ':') != false) {
+            $options = $this->options;
 
-			foreach (explode(':', $option) as $key) {
-				if (!isset($options[$key])) {
-					break;
-				}
+            foreach (explode(':', $option) as $key) {
+                if (!isset($options[$key])) {
+                    break;
+                }
 
-				$options = $options[$key];
-			}
+                $options = $options[$key];
+            }
 
-			return $options ?: $default;
-		}
+            return $options ?: $default;
+        }
 
-		return $this->options[$option] ?: $default;
-	}
+        return $this->options[$option] ?: $default;
+    }
 }

ad6563486c646582720e5b8f40b8428aed4e620b
Pascal Iske <info@pascal-iske.de>
2018-08-04 12:00:44 +0200

    refactor: refactor plugin for easier usage

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -1,19 +1,10 @@
 <?php
 namespace PI\Configuration;
 
-use ComposerLocator;
 use Symfony\Component\Yaml\Yaml;
 
 class Configuration {
     /** --- constants --- **/
-
-    /**
-     * The env variables.
-     *
-     * @access private
-     * @var array
-     */
-    private $argv;
 
     /**
      * The root path.
@@ -24,36 +15,20 @@ class Configuration {
     private $root;
 
     /**
-     * The urls.
-     *
-     * @access private
-     * @var UrlSet
-     */
-    private $urls;
-
-    /**
-     * The options.
-     *
-     * @access private
-     * @var array
-     */
-    private $options;
-
-    /**
-     * The resolved hostname.
-     *
-     * @access private
-     * @var string
-     */
-    private $hostname;
-
-    /**
-     * The resolved environment.
+     * The environment.
      *
      * @access private
      * @var string
      */
     private $environment;
+
+    /**
+     * The values.
+     *
+     * @access private
+     * @var array
+     */
+    private $values;
 
     /** --- constructor --- **/
 
@@ -64,82 +39,15 @@ class Configuration {
      * @param UrlSet $urls
      * @return void
      */
-    public function __construct(UrlSet $urls) {
-        global $argv;
+    public function __construct($root, $environment = 'production') {
+        $this->root = $root;
+        $this->environment = $environment;
+        $this->values = array();
 
-        $this->argv = $argv ?: array();
-        $this->root = ComposerLocator::getRootPath();
-        $this->urls = $urls;
-
-        $this->options = array();
-        $this->resolveHostname();
-        $this->resolveEnvironment();
         $this->resolveConfig();
     }
 
     /** --- private --- **/
-
-    /**
-     * Resolves the hostname.
-     *
-     * @access private
-     * @return void
-     */
-    private function resolveHostname() {
-        $hostname = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
-
-        // fetch hostname
-        if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && !empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-            $hostname = $_SERVER['HTTP_X_FORWARDED_HOST'];
-        }
-
-        // are we in a cli?
-        if ((PHP_SAPI == 'cli' || php_sapi_name() == 'cli')) {
-            $hostname = 'cli';
-        }
-
-        // ignore ports on urls
-        if (strpos($hostname, ':') !== false) {
-            $hostname = substr($hostname, 0, strpos($hostname, ':'));
-        }
-
-        $this->hostname = $hostname;
-        $this->options['hostname'] = $hostname;
-    }
-
-    /**
-     * Resolves the environment.
-     *
-     * @access private
-     * @return void
-     */
-    private function resolveEnvironment() {
-        $environment = 'production';
-
-        // try env var
-        if (getenv('WP_ENV') !== false) {
-            $environment = preg_replace('/[^a-z]/', '', getenv('WP_ENV'));
-        }
-
-        // try env from hostnames
-        foreach (array('production', 'staging', 'development') as $env) {
-            if (in_array($this->hostname, $this->urls->get($env))) {
-                $environment = $env;
-            }
-        }
-
-        // try cli arguments
-        if ((PHP_SAPI == 'cli' || php_sapi_name() == 'cli')) {
-            foreach ($this->argv as $arg) {
-                if (preg_match('/--env=(.+)/', $arg, $match)) {
-                    $environment = $match[1];
-                }
-            }
-        }
-
-        $this->environment = $environment;
-        $this->options['environment'] = $environment;
-    }
 
     /**
      * Resolves all config files.
@@ -157,34 +65,34 @@ class Configuration {
             $data[$filename] = Yaml::parse(file_get_contents($file));
         }
 
-        $this->options = array_merge($this->options, $data);
+        $this->values = array_merge($this->values, $data);
     }
 
     /** --- public --- **/
 
     /**
-     * Returns the value for the given key.
+     * Returns the value for the given key path.
      *
      * @access public
-     * @param  string $option
+     * @param  string $path
      * @param  mixed $default
      * @return mixed
      */
-    public function get($option, $default = null) {
-        if (strpos($option, ':') != false) {
-            $options = $this->options;
+    public function get($path, $default = null) {
+        if (strpos($path, ':') != false) {
+            $values = $this->values;
 
-            foreach (explode(':', $option) as $key) {
-                if (!isset($options[$key])) {
+            foreach (explode(':', $path) as $key) {
+                if (!isset($values[$key])) {
                     break;
                 }
 
-                $options = $options[$key];
+                $values = $values[$key];
             }
 
-            return $options ?: $default;
+            return $values ?: $default;
         }
 
-        return $this->options[$option] ?: $default;
+        return $this->values[$path] ?: $default;
     }
 }
